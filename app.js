00ef100eb4f35e27f6a1a4c561b1c851dfaef708
Devin Ford <devindford@att.net>
2020-09-24 02:02:35 -0400

    updated copyright JS to include if statement for after 2020 to include 2020 - new date

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-document
-  .getElementById("copyright")
-  .appendChild(document.createTextNode(new Date().getFullYear()));
+document.getElementById("copyright")
+
+  if (new Date().getFullYear() === 2020) {
+    copyright.appendChild(document.createTextNode(' 2020'));
+  } else {
+    copyright.appendChild(document.createTextNode(' 2020 - ' + new Date().getFullYear()));
+  };
